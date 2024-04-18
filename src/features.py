cc9bd9c5dce87ced6be0c291bf3694a3a75d1077
connordilgren <connord0127@gmail.com>
2024-04-17 23:51:26 -0500

    basic mfcc with spectrogram plot
    to be used for progress report demo on 4/18
    needs unit tests
    needs docstring

--- src/features.py
+++ src/features.py
@@ -1,59 +1,60 @@
+import librosa
 import matplotlib.pyplot as plt
 import numpy as np
-import librosa
+import os
 
 
-def get_librosa_example_audio(key: str):
-    """Gets signal and sampling rate from librosa example file
+def mfcc(audio_file_path: str,
+         sr_target: int,
+         n_fft: int,
+         hop_length: int,
+         n_mels: int,
+         filter_name: str,
+         plot: bool = False):
 
-    Args:
-        key (str): key for librosa example audio file. For a full list of
-        keys, see:
-        https://librosa.org/doc/latest/recordings.html
-    """
-    y, sr = librosa.load(librosa.ex(key))
-    return y, sr
+    y, sr = librosa.load(audio_file_path, sr=sr_target)
+    win_func = librosa.filters.get_window(filter_name, n_fft)
 
+    mfccs = librosa.feature.mfcc(y=y,
+                                 sr=sr,
+                                 n_fft=n_fft,
+                                 hop_length=hop_length,
+                                 n_mels=n_mels,
+                                 window=win_func)
 
-def get_true():
-    return True
+    if plot is True:
+        S = librosa.feature.melspectrogram(y=y,
+                                           sr=sr,
+                                           n_fft=n_fft,
+                                           hop_length=hop_length,
+                                           n_mels=n_mels,
+                                           window=win_func)
 
+        fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(12, 12))
+        img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max),
+                                       x_axis='time', y_axis='mel', fmax=8000,
+                                       ax=ax[0])
+        fig.colorbar(img, ax=[ax[0]])
+        ax[0].set(title='Mel spectrogram')
+        ax[0].label_outer()
+        img = librosa.display.specshow(mfccs, x_axis='time', ax=ax[1])
+        fig.colorbar(img, ax=[ax[1]])
+        ax[1].set(title='MFCC')
+        plt.savefig(os.path.join("figures", "temp", "spectrogram_mfccs.png"))
 
-# get power spectrogram
-def power_spectrum(y, sr):
-    D = np.abs(librosa.stft(y))**2
-    return D
-
-
-# get mel spectrogram
-def mel_spectrogram(y, sr):
-    S = librosa.feature.melspectrogram(y=y, sr=sr)
-    return S
-
-
-# get MFCCs
-def mfcc(y, sr):
-    M = librosa.feature.mfcc(y, sr=sr)
-    return M
-
-
-# plot mel spectrogram
-def plot_mel_spectrogram(S, sr):
-    fig, ax = plt.subplots()
-    S_dB = librosa.power_to_db(S, ref=np.max)
-    img = librosa.display.specshow(S_dB, x_axis='time',
-                                   y_axis='mel', sr=sr,
-                                   fmax=8000, ax=ax)
-    fig.colorbar(img, ax=ax, format='%+2.0f dB')
-    ax.set(title='Mel-frequency spectrogram')
+    return mfccs
 
 
-# plot MFCCs
-def plot_mfcc(M):
-    fig, ax = plt.subplots()
-    img = librosa.display.specshow(M, x_axis='time', ax=ax)
-    fig.colorbar(img, ax=ax)
-    ax.set(title='MFCC')
+if __name__ == '__main__':
+    audio_file_path = os.path.join('.', 'training_data', 'DEAM', '2.mp3')
 
+    mfccs = mfcc(audio_file_path=audio_file_path,
+                 sr_target=22050,
+                 n_fft=5512,
+                 hop_length=2205,
+                 n_mels=13,
+                 filter_name='hann',
+                 plot=True
+                 )
 
-# plot mel spectrogram
+    print('done')
